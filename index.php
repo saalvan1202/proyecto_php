65c0b3ef86b0c4012ca2c67d71b7f66bf9a7f156
saalvan1202 <ashande75@gmail.com>
2024-09-30 21:02:41 -0500

    footer

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ curl_close($ch);
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
 >
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <style>
     body{
         display: flex;
@@ -46,6 +47,37 @@ curl_close($ch);
         flex-direction: column;
         width: 80vh;
     }
+    footer{
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        font-size: 30px;
+        justify-content: center;
+        align-items: center;
+        margin-top: 50px;
+        width: 100%;
+        background-color: black;
+        height: 30vh;
+        color: white;
+    }
+    section{
+        padding-top: 50px;
+        padding-bottom: 20px;
+        display: flex;
+        gap: 15px;
+        font-size: 30px;
+        justify-content: center;
+        align-items: center;
+        width: 30%;
+        background-color: black;
+        height: 15vh;
+        color: white;
+        border-bottom: 1px solid white;
+    }
+    
+    h2{
+        font-size: 25px;
+    }
     @media(max-width:800px){
         hgroup{
             margin: 0 20px 0 20px;
@@ -53,6 +85,12 @@ curl_close($ch);
             align-items: start;
             width: 50vh;
         }
+        body{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
     }
 
 </style>
@@ -72,4 +110,11 @@ curl_close($ch);
     ?></p>
 </hgroup>
 </body>
+<footer>
+<section>
+<a href="https://www.facebook.com/ShandeAlvan2004" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+<a href="https://www.instagram.com/alvan_saa/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+<a href="https://github.com/saalvan1202/" target="_blank"><i class="fa-brands fa-github"></i></a><br/></section>
+<h2>Shande Andres</h2><br/>       
+</footer>
 </html>
